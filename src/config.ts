f9689e090d51e9bade4423848adfe7617048efad
Natalia <alamo.natalia@gmail.com>
2025-04-11 16:21:09 +0200

    added config.ts file for static configurations

---
+++ src/config.ts
@@ -0,0 +1,27 @@
+export interface SiteConfig {
+    autor: string;
+    autorUrl: string;
+    site: string;
+    siteLegend: string;
+    siteTitle: string;
+    siteDescription: string;
+    siteUrl: string;
+    favicon: string;
+    social: {
+        instagram?: string;
+    }
+}
+
+export const config: SiteConfig = {
+    autor: 'FTN Agency',
+    autorUrl: 'https://wwww.ftn.com',
+    site: 'Company name',
+    siteLegend: 'Digital solutions for entrepreneurs and modern businesses.',
+    siteTitle: 'Landing page for startups',
+    siteDescription: 'The company sells a lot of things ',
+    siteUrl: 'https://wwww.namecompany.com/',
+    favicon: '/favicon.svg',
+    social: {
+        instagram: 'lkajsdfjh'
+    },
+}
