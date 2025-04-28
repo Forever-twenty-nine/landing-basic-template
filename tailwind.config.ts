99f8cc6a8695b725128e3ef5bc757025e03fe140
Sebas Echazú <sebastianechazu@outlook.com>
2025-04-28 18:12:31 -0300

    feat: migrate from local astro-ui-lib to @forever-twenty-nine/astro-ui-lib
    - Updated package.json to include @forever-twenty-nine/astro-ui-lib as a dependency.
    - Refactored imports in Layout.astro and index.astro to use the new library.
    - Removed references to local astro-ui-lib components.
    - Updated tailwind.config.ts to include the new library's components in the content path.
    - Cleaned up global.css by removing the import for the old library's global components.
    - Added .npmrc for GitHub package registry authentication.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,19 +1,11 @@
 import type { Config } from 'tailwindcss';
-import { join } from 'path'
-
 import { config as siteConfig } from './src/config';
 
 const config: Config = {
   content: [
-    './src/**/*.{astro,html,js,jsx,ts,tsx,mdx}',
-
-    // RUTA HACIA TU LIBRERÍA DE COMPONENTES
-    // Si está como paquete npm local:
-    // './node_modules/astro-ui-lib/**/*.{astro,js,ts,jsx,tsx}',
-    
-    // O si está en forma de folder (desarrollo local con pnpm/yarn/npm link)
-    join(__dirname, '../astro-ui-lib/components/**/*.{astro,js,ts,jsx,tsx}'),
-    './src/styles/**/*.{css}'
+    './src/**/*.{astro,html,js,jsx,ts,tsx,mdx}', // Archivos dentro de src
+    './src/styles/**/*.{css}', // Archivos CSS dentro de src
+    './node_modules/@forever-twenty-nine/astro-ui-lib/components/**/*.{astro}',// Archivos de la librería
   ],
   theme: {
     extend: {
@@ -29,4 +21,4 @@ const config: Config = {
   plugins: []
 }
 
-export default config
+export default config;
